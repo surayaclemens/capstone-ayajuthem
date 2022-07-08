b39a24618968e00ac7f513fc4758303e200d2096
Suraya <surayaclemens@gmail.com>
2022-07-07 20:59:49 -0600

    more troubleshooting, trying backticks

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const dataURL = 'http://localhost:5050/phrases';
 
 
-const dataURL = process.env.REACT_APP_SERVER_URL
+const dataURL = `$process.env.REACT_APP_SERVER_URL`
 
 const phrasesAPI = {
     getAll: () => {
